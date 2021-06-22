eda27bd889efacb02cca26e31a5dd9fd8ecd1499
connerdouglass <conner.douglass@gmail.com>
2021-06-22 09:33:58 -0700

    Fixed port number typo

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 # Build the docker image and publish internal port 80 to host port 8080
 # The site will be accessible on port 8080
-docker build -t livestream-api . && docker run --rm -p 8080:80 -it livestream-api
+docker build -t livestream-api . && docker run --rm -p 8080:8080 -it livestream-api
