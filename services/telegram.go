0cc68fb84d66aa60a98d09bea54b53223f7a67df
connerdouglass <conner.douglass@gmail.com>
2021-06-22 21:20:01 -0700

    Added support for photo URLs

--- services/telegram.go
+++ services/telegram.go
@@ -8,12 +8,13 @@ import (
 )
 
 type TelegramUser struct {
-	AuthDate  uint64 `json:"auth_date"`
-	ID        uint64 `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Username  string `json:"username"`
-	Hash      string `json:"hash"`
+	AuthDate  uint64  `json:"auth_date"`
+	ID        uint64  `json:"id"`
+	FirstName *string `json:"first_name"`
+	LastName  *string `json:"last_name"`
+	PhotoUrl  *string `json:"photo_url"`
+	Username  string  `json:"username"`
+	Hash      string  `json:"hash"`
 }
 
 type TelegramService struct {
@@ -24,18 +25,24 @@ type TelegramService struct {
 // Verify verified the validity of a Telegram user
 func (s *TelegramService) Verify(user *TelegramUser) bool {
 
+	// Add all of the keyval pairs to a slice
+	keyvals := []string{}
+	keyvals = append(keyvals, fmt.Sprintf("auth_date=%d", user.AuthDate))
+	if user.FirstName != nil {
+		keyvals = append(keyvals, fmt.Sprintf("first_name=%s", *user.FirstName))
+	}
+	keyvals = append(keyvals, fmt.Sprintf("hash=%s", user.Hash))
+	keyvals = append(keyvals, fmt.Sprintf("id=%d", user.ID))
+	if user.LastName != nil {
+		keyvals = append(keyvals, fmt.Sprintf("last_name=%s", *user.LastName))
+	}
+	if user.PhotoUrl != nil {
+		keyvals = append(keyvals, fmt.Sprintf("photo_url=%s", *user.PhotoUrl))
+	}
+	keyvals = append(keyvals, fmt.Sprintf("username=%s", user.Username))
+
 	// Create the data check string
-	dataCheckString := strings.Join(
-		[]string{
-			fmt.Sprintf("auth_date=%d", user.AuthDate),
-			fmt.Sprintf("first_name=%s", user.FirstName),
-			fmt.Sprintf("hash=%s", user.Hash),
-			fmt.Sprintf("id=%d", user.ID),
-			fmt.Sprintf("last_name=%s", user.LastName),
-			fmt.Sprintf("username=%s", user.Username),
-		},
-		"\n",
-	)
+	dataCheckString := strings.Join(keyvals, "\n")
 
 	// Create the hash of the API key
 	secretKey := utils.Sha256Hex(s.BotAPIKey)
